5d68e6dcdcc7c0dbc0e4ca25063108b38c399c74
Chandan P <32537337+Chandanps@users.noreply.github.com>
2021-07-12 22:45:14 +0530

    commit 1: nothing much

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
         <h3>Some random header</h3>
       </header>
       <div className="App-content">
-        If all of this is displayed, a demo deployment of react-app on github is successfull!!
+        Hello Sonali and Shashank!<br/>
+        :P <br/>
+
       </div>
       <footer className="App-footer">
         Some random footer
